79e1687985557a5a5fb7aafdb4add8377cee58b8
linusjuni <s225224@student.dtu.dk>
2024-06-07 14:43:58 +0200

    new project

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use("/", (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
